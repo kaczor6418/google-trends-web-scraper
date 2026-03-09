4318f1804c22bd0a9a1a15e76c12752b9842956f
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2026-03-09 18:22:38 +0100

    chore: accept search as file path param

--- scrapper/src/extract-data-from-search.js
+++ scrapper/src/extract-data-from-search.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { startScraping } from './scraper/trendsScraper.js';
@@ -7,15 +8,22 @@ dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const searchMovie = [
-  { 
-    phrase: "Harry Potter", 
-    timeframes: ["LAST_24_HOURS", { start: "11-02-2026", end: "18-02-2026" }], 
-    geos: ["PL"] 
+/**
+ * Loads the search configuration from a JSON file.
+ * Defaults to 'search_queries.json' if no argument is provided.
+ */
+function loadQueries() {
+  const filePath = process.argv[2] || path.join(__dirname, '../search_queries.json');
+  
+  if (!fs.existsSync(filePath)) {
+    console.error(`[FATAL] Input file not found: ${filePath}`);
+    process.exit(1);
   }
-];
+  
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+}
 
-
+// Configuration object
 const config = {
   chromeUrl: process.env.CHROME_DEBUG_URL || 'http://localhost:9222',
   apiUrl: process.env.BACKEND_API_URL,
@@ -23,4 +31,5 @@ const config = {
   fallbackDir: path.join(__dirname, '../fallbacks')
 };
 
-startScraping(searchMovie, config);
+const searchData = loadQueries();
+startScraping(searchData, config);
